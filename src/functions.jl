e925706edd9ce21788f0f5014b57a0f76dd918c0
Markus Pirke <markus.pirke@web.de>
2022-06-20 19:50:20 +0200

    maybe forgot save before

--- src/functions.jl
+++ src/functions.jl
@@ -16,3 +16,20 @@ function energyspectrum_BIG(H, n::Int64, ϵ)
     return BigFloat.(E)
 end
 
+
+"""
+Function for sorting string in 1,2,3,4,5,6,7,8,9,10,11 ... 
+"""
+function natural(x, y)
+    k(x) = [occursin(r"\d+", s) ? parse(Int, s) : s
+            for s in split(replace(x, r"\d+" => s -> " $s "))]
+    A = k(x)
+    B = k(y)
+    for (a, b) in zip(A, B)
+        if !isequal(a, b)
+            return typeof(a) <: typeof(b) ? isless(a, b) :
+                   isa(a, Int) ? true : false
+        end
+    end
+    return length(A) < length(B)
+end
